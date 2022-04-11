9b452a5ac40d0fcd0ec026b9ee10df9b933127bd
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-11 13:23:50 +0100

    created model and controller for flat and started to write the index action and displaying the data on the view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'destroing all the flat'
+Flat.destroy_all if Rails.env.development?
+
+puts 'db clean'
+
+puts 'strat creating flats'
+
+5.times do
+  flat = Flat.create!(
+    name: "house of #{Faker::FunnyName.two_word_name}",
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: (10..200).to_a.sample,
+    number_of_guest: (1..5).to_a.sample,
+  )
+
+  puts "created id #{flat.id} #{flat.name}"
+end
+
+
